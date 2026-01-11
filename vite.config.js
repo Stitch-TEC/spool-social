5bba63e9eb8fe64c0371b4130afb6e60d9bea406
Dillon Faulkner <Kist221@gmail.com>
2026-01-10 20:10:50 -0700

    fixed vite config path to relative

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spool-app/', // <--- IMPORTANT: This must match your GitHub Repo name!
+  base: './', // <--- IMPORTANT: This must match your GitHub Repo name or be relative path
 })
