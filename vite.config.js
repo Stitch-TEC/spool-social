5ede04ac1c3d189b37f543137391b417e65e6c08
Dillon Faulkner <Kist221@gmail.com>
2026-01-10 20:14:55 -0700

    correct to absolute link of repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- IMPORTANT: This must match your GitHub Repo name or be relative path
+  base: '/spool-social/', // <--- IMPORTANT: This must match your GitHub Repo name
 })
